c9e866b96a28b9c419f66a6e0e32138b8e4398ea
Rich <r.j.lewindon@hotmail.co.uk>
2020-08-11 10:00:01 +0100

    Init tab and viewpager in main activity

--- app/src/main/java/com/example/android/swimapp/MainActivity.java
+++ app/src/main/java/com/example/android/swimapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.swimapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        TabItem findSessions = findViewById(R.id.findSessionTab);
+        TabItem shareSessions = findViewById(R.id.shareSessionTab);
+        ViewPager2 viewPager2 = findViewById(R.id.myViewPager);
     }
 }
