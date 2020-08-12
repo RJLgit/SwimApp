aa40debd5e93b520d28e33abdf09040f911517ad
Rich <r.j.lewindon@hotmail.co.uk>
2020-08-12 09:36:35 +0100

    Created set fragment to hold creation of new set ui

--- app/src/main/java/com/example/android/swimapp/SetFragment.java
+++ app/src/main/java/com/example/android/swimapp/SetFragment.java
@@ -4,16 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ShareFragment extends Fragment {
-    private static final String TAG = "ShareFragment";
+public class SetFragment extends Fragment {
 
-    public ShareFragment() {
+
+
+    public SetFragment() {
         // Required empty public constructor
     }
 
@@ -21,8 +21,7 @@ public class ShareFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d(TAG, "onCreateView: ");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_share, container, false);
+        return inflater.inflate(R.layout.fragment_set, container, false);
     }
 }
