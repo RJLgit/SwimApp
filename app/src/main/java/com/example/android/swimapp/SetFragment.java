6faae6b95014782ce06e82e040a1d56aff14e7c6
Rich <r.j.lewindon@hotmail.co.uk>
2020-08-12 09:52:43 +0100

    Clicking add set now shows edit text and label

--- app/src/main/java/com/example/android/swimapp/SetFragment.java
+++ app/src/main/java/com/example/android/swimapp/SetFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class SetFragment extends Fragment {
@@ -21,7 +24,22 @@ public class SetFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_set, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set, container, false);
+        final Button addSetButton = view.findViewById(R.id.addNewSetButton);
+        final TextView enterSetLabel = view.findViewById(R.id.enterSetLabel);
+        final EditText enterSetEditText = view.findViewById(R.id.addSetEditText);
+
+        addSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addSetButton.setVisibility(View.INVISIBLE);
+                enterSetLabel.setVisibility(View.VISIBLE);
+                enterSetEditText.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+        return view;
     }
 }
